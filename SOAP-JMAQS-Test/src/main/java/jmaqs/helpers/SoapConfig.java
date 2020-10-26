f2570f5b418f4b009146bb481ab0f1656263e4b4
Jason Edstrom <jason@instacode.io>
2020-10-26 13:15:31 -0500

    Large amount of the abstraction is working

--- SOAP-JMAQS-Test/src/main/java/jmaqs/helpers/SoapConfig.java
+++ SOAP-JMAQS-Test/src/main/java/jmaqs/helpers/SoapConfig.java
@@ -1,12 +1,27 @@
 package jmaqs.helpers;
 
+import com.magenic.jmaqs.utilities.helper.Config;
+
+import java.util.Map;
+
 public final class SoapConfig{
 
     private static final String SOAP_SECTION = "SoapMaqs";
+    private static final String SOAP_NAMESPACE_SECTION = "SoapMaqsNameSpaces";
 
     private SoapConfig()
     {
 
+    }
+
+    public static String getSoapPrefix()
+    {
+        return Config.getValueForSection(SOAP_SECTION, "SoapPrefix");
+    }
+
+    public static Map<String, String> getSoapNamespaces()
+    {
+        return Config.getSection(SOAP_NAMESPACE_SECTION);
     }
 
 
--- SOAP-JMAQS-Test/src/test/java/org/example/test/WebServiceRestTest.java
+++ SOAP-JMAQS-Test/src/test/java/org/example/test/WebServiceRestTest.java
@@ -2,6 +2,8 @@ package org.example.test;
 
 import com.magenic.jmaqs.utilities.logging.MessageType;
 import com.magenic.jmaqs.webservices.jdk8.BaseWebServiceTest;
+import com.magenic.jmaqs.webservices.jdk8.WebServiceUtilities;
+import jmaqs.helpers.SoapConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -9,8 +11,6 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.example.models.countries.GetCountryRequest;
-import org.springframework.ws.soap.SoapEnvelope;
-import org.springframework.ws.soap.SoapMessage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.w3c.dom.Document;
@@ -24,16 +24,24 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.soap.*;
-import javax.xml.validation.Schema;
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.StringReader;
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.function.Consumer;
 
 /**
  * The type Web service test.
  */
 public class WebServiceRestTest extends BaseWebServiceTest {
 
+  private final MessageFactory messageFactory = MessageFactory.newInstance();
+  private final SOAPMessage soapMessage = messageFactory.createMessage();
 
+  public WebServiceRestTest() throws SOAPException {
+  }
 
   @Test
   public void SOAPJMAQSXMLStringTest() throws IOException, URISyntaxException, JAXBException {
@@ -103,44 +111,125 @@ public class WebServiceRestTest extends BaseWebServiceTest {
     this.getLogger().logMessage(MessageType.INFORMATION, "Starting data manipulations");
     GetCountryRequest getCountryRequest = new GetCountryRequest();
     getCountryRequest.setName("Spain");
-    this.getLogger().logMessage(MessageType.INFORMATION, "Starting marshalling process");
-    JAXBContext jaxbContext = JAXBContext.newInstance(GetCountryRequest.class);
-    Marshaller marshaller = jaxbContext.createMarshaller();
-    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-    OutputStream outputStream = new ByteArrayOutputStream();
-    marshaller.marshal(getCountryRequest, outputStream);
-    outputStream.flush();
-    outputStream.close();
-    this.getLogger().logMessage(MessageType.INFORMATION, "Completed marshalling");
-    this.getLogger().logMessage(MessageType.VERBOSE, outputStream.toString());
+    String outputStream = createSoapPayload(getCountryRequest);
 
     this.getLogger().logMessage(MessageType.INFORMATION, "Crafting SOAP Message");
-    MessageFactory messageFactory = MessageFactory.newInstance();
-    SOAPMessage message = messageFactory.createMessage();
-    SOAPEnvelope envelope = message.getSOAPPart().getEnvelope();
-    envelope.removeNamespaceDeclaration(envelope.getPrefix());
-    envelope.addNamespaceDeclaration("gs","http://spring.io/guides/gs-producing-web-service");
-    envelope.addNamespaceDeclaration("soapenv","http://schemas.xmlsoap.org/soap/envelope/");
-    envelope.setPrefix("soapenv");
-    SOAPBody body = envelope.getBody();
-    SOAPHeader header = envelope.getHeader();
-    header.setPrefix("soapenv");
-    body.setPrefix("soapenv");
-    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-    documentBuilderFactory.setNamespaceAware(true);
-    documentBuilderFactory.setSchema(marshaller.getSchema());
-    DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-    Document document = documentBuilder.parse(new InputSource(new StringReader(outputStream.toString())));
-    body.addDocument(document);
-    message.saveChanges();
-    OutputStream messageOutputStream = new ByteArrayOutputStream();
-    message.writeTo(messageOutputStream);
+    SOAPEnvelope envelope = modifySoapPrefix(soapMessage.getSOAPPart().getEnvelope());
+    addNameSpaceToMessage(envelope, "gs","http://spring.io/guides/gs-producing-web-service");
+    addStringToBody(outputStream, soapMessage);
+    String messageOutputStream = finalizeSoapMessage(soapMessage);
     this.getLogger().logMessage(MessageType.INFORMATION, "Completed and saved SOAP Message changes");
-    this.getLogger().logMessage(MessageType.VERBOSE, messageOutputStream.toString());
-    StringEntity entity = new StringEntity(messageOutputStream.toString(), ContentType.TEXT_XML);
+    this.getLogger().logMessage(MessageType.VERBOSE, messageOutputStream);
+    StringEntity entity = new StringEntity(messageOutputStream, ContentType.TEXT_XML);
     CloseableHttpResponse closeableHttpResponse = this.getTestObject().getWebServiceDriver()
         .postContent("/ws", entity, ContentType.TEXT_XML, false);
     Assert.assertEquals(closeableHttpResponse.getStatusLine().getStatusCode(), 200);
+  }
+
+  @Test
+  public void SOAPJMAQSUsingOverloadsTest() throws JAXBException, IOException, SOAPException, ParserConfigurationException, SAXException, URISyntaxException {
+    this.getLogger().logMessage(MessageType.INFORMATION, "Starting data manipulations");
+    GetCountryRequest getCountryRequest = new GetCountryRequest();
+    getCountryRequest.setName("Spain");
+    String outputStream = createSoapPayload(getCountryRequest);
+    String messageOutputStream = getDefaultSoapMessage(outputStream);
+    StringEntity entity = WebServiceUtilities.createStringEntity(messageOutputStream, ContentType.TEXT_XML);
+    CloseableHttpResponse closeableHttpResponse = this.getTestObject().getWebServiceDriver()
+            .postContent("/ws", entity, ContentType.TEXT_XML, false);
+    Assert.assertEquals(closeableHttpResponse.getStatusLine().getStatusCode(), 200);
+  }
+
+  private String getDefaultSoapMessage(String outputStream) throws SOAPException, ParserConfigurationException, SAXException, IOException {
+    //this.getLogger().logMessage(MessageType.INFORMATION, "Crafting SOAP Message");
+    modifySoapPrefix();
+    addNameSpaceCollectionToMessage((HashMap<String, String>) SoapConfig.getSoapNamespaces());
+    addStringToBody(outputStream);
+    String messageOutputStream = finalizeSoapMessage();
+    //this.getLogger().logMessage(MessageType.INFORMATION, "Completed and saved SOAP Message changes");
+    //this.getLogger().logMessage(MessageType.VERBOSE, messageOutputStream);
+    return messageOutputStream;
+  }
+
+  private String finalizeSoapMessage() throws SOAPException, IOException {
+    return finalizeSoapMessage(soapMessage);
+  }
+
+  private String finalizeSoapMessage(SOAPMessage message) throws SOAPException, IOException {
+    message.saveChanges();
+    OutputStream messageOutputStream = new ByteArrayOutputStream();
+    message.writeTo(messageOutputStream);
+    return messageOutputStream.toString();
+  }
+
+  private void addStringToBody(String outputStream) throws ParserConfigurationException, SAXException, IOException, SOAPException {
+    addStringToBody(outputStream, soapMessage);
+  }
+
+  private void addStringToBody(String outputStream, SOAPMessage message) throws ParserConfigurationException, SAXException, IOException, SOAPException {
+    Document document = getDocument(outputStream);
+    message.getSOAPBody().addDocument(document);
+  }
+
+  private void addNameSpaceToMessage(String key, String value) throws SOAPException {
+    addNameSpaceToMessage(soapMessage.getSOAPPart().getEnvelope(), key, value);
+  }
+
+  private void addNameSpaceToMessage(SOAPEnvelope envelope, String key, String value) throws SOAPException {
+    envelope.addNamespaceDeclaration(key, value);
+  }
+
+  private void addNameSpaceCollectionToMessage(HashMap<String, String> map) throws SOAPException {
+    addNameSpaceCollectionToMessage(soapMessage.getSOAPPart().getEnvelope(), map);
+  }
+
+  private void addNameSpaceCollectionToMessage(SOAPEnvelope envelope, HashMap<String, String> map) {
+    Consumer consumer = (key) -> {
+      String value = map.get(key);
+      try {
+        //this.getLogger().logMessage(MessageType.INFORMATION, "Adding namespace to message: key: %s, value: %s", key, value);
+        addNameSpaceToMessage(envelope, (String) key, value);
+      } catch (SOAPException e) {
+        e.printStackTrace();
+      }
+    };
+    map.keySet().stream().iterator().forEachRemaining(consumer);
+  }
+
+
+  private String createSoapPayload(GetCountryRequest getCountryRequest) throws JAXBException, IOException {
+    //this.getLogger().logMessage(MessageType.INFORMATION, "Starting marshalling process");
+    OutputStream outputStream = new ByteArrayOutputStream();
+    JAXBContext jaxbContext = JAXBContext.newInstance(GetCountryRequest.class);
+    Marshaller marshaller = jaxbContext.createMarshaller();
+    marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+    marshaller.marshal(getCountryRequest, outputStream);
+    outputStream.flush();
+    outputStream.close();
+    //this.getLogger().logMessage(MessageType.INFORMATION, "Completed marshalling");
+    //this.getLogger().logMessage(MessageType.VERBOSE, outputStream.toString());
+    return outputStream.toString();
+  }
+
+  private Document getDocument(String bodyContent) throws ParserConfigurationException, SAXException, IOException {
+    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+    documentBuilderFactory.setNamespaceAware(true);
+    DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+    return documentBuilder.parse(new InputSource(new StringReader(bodyContent)));
+  }
+
+  private SOAPEnvelope modifySoapPrefix() throws SOAPException {
+    return modifySoapPrefix(soapMessage.getSOAPPart().getEnvelope());
+  }
+
+
+  private SOAPEnvelope modifySoapPrefix(SOAPEnvelope envelope) throws SOAPException {
+    //this.getLogger().logMessage(MessageType.INFORMATION, "removing default prefix: %s", envelope.getPrefix());
+    envelope.removeNamespaceDeclaration(envelope.getPrefix());
+    addNameSpaceToMessage(envelope, SoapConfig.getSoapPrefix(), "http://schemas.xmlsoap.org/soap/envelope/");
+    envelope.setPrefix(SoapConfig.getSoapPrefix());
+    envelope.getBody().setPrefix(SoapConfig.getSoapPrefix());
+    envelope.getHeader().setPrefix(SoapConfig.getSoapPrefix());
+    return envelope;
   }
 
 
